a46f98c3fbbbccbfba705ab3af2c6b361f9c5adb
David Majercak <davidmajercak@gmail.com>
2020-06-30 21:10:49 -0400

    More small fixes. Game is posted a couple places

--- Assets/JS/Worker.js
+++ Assets/JS/Worker.js
@@ -1,5 +1,5 @@
 Worker.prototype.getTotalPower = function() { 
-	return this.baseSipSize * this.owned * player.workerProductionBonus * caffeineSacrificeProductionBonus;
+	return this.baseSipSize * this.owned * player.workerProductionBonus * player.caffeineSacrificeProductionBonus;
 }
 
 Worker.prototype.increaseSipSize = function(increaseMultiplier) {
